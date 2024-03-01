8995a78a66b8d08a527cde8890c50fc8bd4d2f74
SailorMusic05 <141599762+SailorMusic05@users.noreply.github.com>
2024-03-01 11:20:47 -0500

    Cargadno Estrella Controller

---
+++ PrimeraEntregaWeb-aa995a8393e7c6709d367a22bdd8896c6cf75740/src/main/java/com/example/PrimeraEntregaWeb/controller/EstrellaController.java
@@ -0,0 +1,76 @@
+package com.example.PrimeraEntregaWeb.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.PrimeraEntregaWeb.model.Estrella;
+import com.example.PrimeraEntregaWeb.services.EstrellaService;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/estrella")
+public class EstrellaController {
+    Logger loggy = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private EstrellaService estrellitaService;
+
+    @GetMapping("/list")
+    public String listarEstrellas(Model model){
+        List<Estrella> estrellitas = estrellitaService.listarEstrellas();
+        loggy.info("estrella" + estrellitas.size());
+        model.addAttribute("estrella",estrellitas);
+        return "estrella-list";
+    }
+
+     @GetMapping("/view/{id}")
+     String verEstrellas(Model model, @PathVariable("id") Long id){
+        Estrella estrellita = estrellitaService.buscar(id);
+        loggy.info("estrella" + estrellita);
+
+        model.addAttribute("estrella", estrellita);
+        return "estrella-edit";
+     }
+
+     @GetMapping("/edit-form/{id}")
+     public String formularioEditarEstrella(Model model, @PathVariable Long id){
+        Estrella estrellita = estrellitaService.buscar(id);
+        model.addAttribute("estrella", estrellita);
+        return "estrella-edit";
+     }
+
+     @PostMapping(value = "/save")
+     public String guardarNave(@Valid Estrella estrellita, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "estrella-edit";
+        }
+        estrellitaService.guardarNave(estrellita);
+        return "redirect:/estrella/list";
+     }
+
+     @GetMapping("/search")
+     public String listaNaves(@RequestParam(required = false) String searchText, Model model){
+        List<Estrella> estrellitas;
+        if(searchText == null || searchText.trim().equals("")){
+            loggy.info("No hay texto de búsqueda, retornando todo");
+            estrellitas = estrellitaService.listarEstrellas();
+            model.addAttribute("estrella", estrellitas);
+        }
+
+    return "estrella-search";
+     }
+
+}
--- PrimeraEntregaWeb-aa995a8393e7c6709d367a22bdd8896c6cf75740/src/main/java/com/example/PrimeraEntregaWeb/services/EstrellaService.java
+++ PrimeraEntregaWeb-aa995a8393e7c6709d367a22bdd8896c6cf75740/src/main/java/com/example/PrimeraEntregaWeb/services/EstrellaService.java
@@ -1,5 +1,31 @@
 package com.example.PrimeraEntregaWeb.services;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.PrimeraEntregaWeb.repository.EstrellaRepository;
+
+import io.micrometer.common.lang.NonNull;
+
+import com.example.PrimeraEntregaWeb.model.Estrella;
+
+@Service
 public class EstrellaService {
+    @Autowired
+    private EstrellaRepository estrellaRepositorio;
+     public List<Estrella> listarEstrellas() {
+        return estrellaRepositorio.findAll();
+    }
+    @SuppressWarnings("null")
+    public Estrella buscar(@NonNull Long id) {
+        return estrellaRepositorio.findById(id).orElseThrow();
+    }
+    public void guardarNave(Estrella estrellita) {
+        estrellaRepositorio.save(estrellita);
+    }
+    public void eliminarNave(Long id) {
+        estrellaRepositorio.deleteById(id);
+    }
 
 }
